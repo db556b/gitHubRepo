f289ae1ce89d76a594c1ef6af2209eca99ce0330
db556b <102768499+db556b@users.noreply.github.com>
2022-04-21 15:13:23 -0400

    updates to BlakcJack App
    Updated BlackJack. Added functionality to extract the value of the cards from the API JSON response and save them in a cumulative variable.

--- blackJack/JS/main.js
+++ blackJack/JS/main.js
@@ -1,42 +1,53 @@
 
 //get deck
 let deckId =''
-fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=5`)
+let cardsLeft
+let playerTotal = 0
+let playerTotalWithAce = 0
+let dealerTotal = 0
+let dealerTotalWithAce = 0
+let playerHasAce = false
+let dealerHasAce = false
+fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=10`)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         deckId = data.deck_id
-        
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
+
+
+function newDeck(x){
+  if (x < 15){
+      fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=10`)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      deckId = data.deck_id
+      
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+  }
+}
 //draw starting cards 
-let dealerDownCard 
+
 document.getElementById('deal').addEventListener('click', deal)
 document.getElementById('hit').addEventListener('click', hit)
 document.getElementById('stay').addEventListener('click', stay)
 document.getElementById('reset').addEventListener('click', reset)
-
+let imageSrc = "assets/images/backOfCard.png"
 function deal(){
-    const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`
+    const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=3`
     fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
         console.log(data)
-        const img0 = document.createElement('img')
-        img0.src = data.cards[0].image
-        document.getElementById('bottom').appendChild(img0)
-        const img1 = document.createElement('img')
-        img1.src = data.cards[1].image
-        document.getElementById('bottom').appendChild(img1)
-        const img3 = document.createElement('img')
-        img3.setAttribute('id', 'dealerFirstCard')
-        img3.src = "assets/images/backOfCard.png"
-        document.getElementById('top').appendChild(img3)
-        const img2 = document.createElement('img')
-        img2.src = data.cards[2].image
-        document.getElementById('top').appendChild(img2)
-        dealerDownCard = data.cards[3]
+        populateDeal(data)
+ //       populateScores(data)
+        loopThroughCards(data.cards)
+        cardsLeft = data.remaining
     })
         .catch(err => {
         console.log(`error ${err}`)
@@ -53,6 +64,8 @@ function hit(){
         const img = document.createElement('img')
         img.src = data.cards[0].image
         document.getElementById('bottom').appendChild(img) 
+ //       populateScores(data)
+        cardsLeft = data.remaining
     })
         .catch(err => {
         console.log(`error ${err}`)
@@ -60,26 +73,88 @@ function hit(){
 }
 //once stay have dealer down card revealed and hit until dealerTotal > 17
 function stay(){
+
     const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
     fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
         console.log(data)
-        document.getElementById('dealerFirstCard').src = dealerDownCard.image
-        const img = document.createElement('img')
-        img.src = data.cards[0].image
-        document.getElementById('top').appendChild(img) 
+        if (imageSrc === "assets/images/backOfCard.png" ) {
+            imageSrc = data.cards[0].image
+            document.getElementById('dealerFirstCard').src = data.cards[0].image
+        } else {
+            const img = document.createElement('img')
+            img.src = data.cards[0].image
+            document.getElementById('top').appendChild(img) 
+            cardsLeft = data.remaining
+ //           populateScores(data)
+        }
     })
         .catch(err => {
         console.log(`error ${err}`)
 });
 }
-//function to extract values of cards
+function populateDeal(data){
+    imageSrc = "assets/images/backOfCard.png"
+    const img0 = document.createElement('img')
+    img0.src = data.cards[0].image
+    document.getElementById('bottom').appendChild(img0)
+    const img1 = document.createElement('img')
+    img1.src = data.cards[1].image
+    document.getElementById('bottom').appendChild(img1)
+    const img3 = document.createElement('img')
+    img3.setAttribute('id', 'dealerFirstCard')
+    img3.src = imageSrc
+    document.getElementById('top').appendChild(img3)
+    const img2 = document.createElement('img')
+    img2.src = data.cards[2].image
+    document.getElementById('top').appendChild(img2)
+}
 
+//function to extract values of cards
+function getValues(x){
+    console.log(x)
+    if (x === 'KING' || x === 'QUEEN' || x === 'JACK'){
+        return 10
+        } else if (x === 'ACE') {
+            return 11
+        } else {
+            return Number(x)
+        }
+    
+}
+
+function loopThroughCards(object){
+    for (let i = 0; i < object.length; i++){
+       let value = getValues(object[i].value)
+        console.log(value)
+        if (i < 2){
+            if (value === 11){
+                playerHasAce = true
+                playerTotal += 1
+                playerTotalWithAce += 11
+            } else {
+                playerTotal += value
+                playerTotalWithAce += value
+            }
+        } else {
+            if (value === 11){
+                dealerdealerHasAce = true
+                dealerTotal += 1
+                dealerTotalWithAce += 11
+            } else {
+                dealerTotal += value
+                dealerTotalWithAce += value
+        }
+    }
+    console.log(dealerTotal,playerTotal, playerTotalWithAce, dealerTotalWithAce)
+    }
+}
 //compare to find winner
 
 //reset playing field
 function reset(){
     document.getElementById('top').replaceChildren()
     document.getElementById('bottom').replaceChildren()
+    newDeck(cardsLeft)
 }
