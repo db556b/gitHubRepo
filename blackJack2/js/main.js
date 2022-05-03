cf7b430afcdaf5ec885aa6e6b12fadb279cb5cc4
db556b <102768499+db556b@users.noreply.github.com>
2022-05-03 10:42:25 -0400

    BlackJack2 update
    made changes to:
    Object model- incorporated game elements and deck elements into the game object.
    Added methods to player constructor to define actions of player and dealer
    Worked out the deckId and incorporated it into the MakeGame constructor

--- blackJack2/js/main.js
+++ blackJack2/js/main.js
@@ -1,38 +1,101 @@
-// creates class for new player
+let dealer
+let player
+let deckId
+let game
+let deck
 
+// creates class for new player
 class MakeGame {
-    constructor (deckId, cardsLeft ){
+    constructor ( deckId, cardsLeft ){
         this.deckId = deckId
         this.cardsLeft = cardsLeft
     }
 
-    // declares methods with player and card (passed as person parameter) classes to add value of card to pertinent total 
+    dealFirstCards(){
+        const url = `https://deckofcardsapi.com/api/deck/${game.deckId}/draw/?count=3`
+        fetch(url)
+            .then(res => res.json()) // parse response as JSON
+            .then(data => {
+            console.log(data)
+            // populateDeal(data)
+            // loopThroughCards(data.cards)
+            this.cardsLeft = data.remaining
 
-    addValueOfCard(person, object){
+        })
+            .catch(err => {
+            console.log(`error ${err}`)
+    });
+
+    }
+
+    newDeck(){
+        if (this.cardsLeft < 15){
+            fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=10`)
+          .then(res => res.json()) // parse response as JSON
+          .then(data => {
+            this.deckId = data.deck_id
+            this.cardsLeft = data.remaining
+ //           localStorage.setItem('deckId', deckId)
+          })
+          .catch(err => {
+              console.log(`error ${err}`)
+          });
+        }
+      }
+
+}
+class MakeDeck{
+    constructor (cardsLeft){
+        this.deckId 
+        this.cardsLeft = cardsLeft
+
+    }
+
+
+}
+
+if(!game){
+    game = new MakeGame(0,0)
+    game.newDeck()
+}
+
+class MakePlayer {
+    constructor(person){
+        this.total = 0
+        this.totalWithAce = 0
+        this.hasAce = false
+        this.person = person
+        this.dom = document.getElementById(`${this.person.toLowerCase()}`)
+        this.currentCard
+        this.finalTotal
+    }
+    
+// declares methods with player and card (passed as person parameter) classes to add value of card to pertinent total 
+    addValueOfCard( object ){
         let value = getValues(object)
-        if (value === 11 && person.hasAce === false){
-        person.hasasAce = true
-        person.total += 1
-        person.totalWithAce += 11
-        console.log(person.total)
-        } else if (value === 11 && person.hasAce === true){
-            person.total +=1
-            person.totalWithAce += 1
+        if (value === 11 && this.hasAce === false){
+        this.hasasAce = true
+        this.total += 1
+        this.totalWithAce += 11
+        console.log(this.total)
+        } else if (value === 11 && this.hasAce === true){
+            this.total +=1
+            this.totalWithAce += 1
         } else {
-        person.total += value
-        person.totalWithAce += value
+        this.total += value
+        this.totalWithAce += value
       //  console.log(playerTotal)
         }
-        if (person.totalWithAce > 21){
-            person.totalWithAce = 1000
+        if (this.totalWithAce > 21){
+            this.totalWithAce = 1000
         }
-        if (person.total > 21){
-            person.total = 1000
+        if (this.total > 21){
+            this.total = 1000
 
     }
     }
 
-    // declares method to extract value of card from fetch 
+// declares method to extract value of card from fetch 
 
     getValues(x){
         console.log(x)
@@ -46,97 +109,35 @@ class MakeGame {
         
     }
 
-    // declares function for use as methods inside the dealer and player classes to update the DOM for player and dealer (passed as arguments)
+// declares function for use as methods inside the dealer and player classes to update the DOM for player and dealer (passed as arguments)
 
     updateDom(person){
-        if (person.total > 100  && person.totalWithAce > 100 ){
-            person.dom.innerText = `${person.person} BUSTS! `
+        if (thisperson.total > 100  && person.totalWithAce > 100 ){
+            person.dom.innerText = `${this.person.toUpperCase()} BUSTS! `
           } else if (person.totalWithAce === 21 || person.total === 21){
-              person.dom.innerText = `${person.person} HAS 21!`
+              person.dom.innerText = `${this.person.toUpperCase()} HAS 21!`
           } else if (person.hasAce === true && person.totalWithAce <= 20) {
-            person.dom.innerText = `${person.person} HAS SOFT ${person.totalWithAce}. `
+            person.dom.innerText = `${this.person.toUpperCase()} HAS SOFT ${person.totalWithAce}. `
           } else {
-              person.dom.innerText = `${person.person} HAS ${person.total}. `
+              person.dom.innerText = `${this.person.toUpperCase()} HAS ${person.total}. `
           }
     }
-// retrieves new deck when the total cards available in the deck reaches less than 15
-
-
-    deal(){
-        this.reset()
-        const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=3`
-        fetch(url)
-            .then(res => res.json()) // parse response as JSON
-            .then(data => {
-            console.log(data)
-            populateDeal(data)
-            loopThroughCards(data.cards)
-            cardsLeft = data.remaining
-
-        })
-            .catch(err => {
-            newDeck(2)
-            deal()
-            console.log(`error ${err}`)
-    });
-
-    }
 }
 
-class MakeDeck {
-    constructor (deckId,cardsLeft){
-        this.deckId = deckId
-        this.cardsLeft = cardsLeft
-    }
-    newDeck(x){
-        if (x < 15){
-            fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=10`)
-          .then(res => res.json()) // parse response as JSON
-          .then(data => {
-            this.deckId = data.deck_id
- //           localStorage.setItem('deckId', deckId)
-          })
-          .catch(err => {
-              console.log(`error ${err}`)
-          });
-        }
-      }
-}
-class MakePlayer {
-    constructor(total, totalWithAce, hasAce){
-        this.total = total
-        this.totalWithAce = totalWithAce
-        this.hasAce = hasAce
-        this.dom = document.getElementById('player')
-        this.person = 'PLAYER'
-        this.currentCard
-        this.finalTotal
-    }
-}
-//creates class for new dealer
 
-class MakeDealer {
-    constructor(total, totalWithAce, hasAce){
-        this.total = total
-        this.totalWithAce = totalWithAce
-        this.hasAce = hasAce
-        this.dom = document.getElementById('dealer')
-        this.person = 'DEALER'
-        this.currentCard
-        this.finalTotal
-    }
-}
-
-const game = new MakeGame (0,0,0)
 
 document.getElementById('deal').addEventListener('click', deal)
-document.getElementById('hit').addEventListener('click', hit)
-document.getElementById('stay').addEventListener('click', stay)
+// document.getElementById('hit').addEventListener('click', hit)
+// document.getElementById('stay').addEventListener('click', stay)
 
 function deal(){
-    const game = new MakeGame(game.deckId, game.cardsLeft)
-    const dealer = new MakeDealer(0,0,false)
-    const player = new MakePlayer(0,0,false) 
+
+    dealer = new MakePlayer(`dealer`)
+    player = new MakePlayer(`player`)
+    game = new MakeGame(game.deckId, game.cardsLeft)
+    if (game.cardsLeft < 15){
+        game.newDeck()
+        } 
 }
 
 
