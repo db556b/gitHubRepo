62457132bde9d27baaa152e8af2cd39a6e363e09
db556b <davidbuchan900@gmail.com>
2022-09-21 10:15:10 -0400

    workinh through the modules

--- job-board/job-board/server/resolvers.js
+++ job-board/job-board/server/resolvers.js
@@ -2,11 +2,16 @@ import { Job, Company } from './db.js';
 
 export const resolvers = {
     Query: {
-        job: (root, { id } ) => Job.findById(id),
+        job: (_root, { id } ) => Job.findById(id),
         jobs : () => Job.findAll(),
+        company:  (_root, { id } ) => Company.findById( id )
     },
 
     Job: {
         company: (job) =>  Company.findById(job.companyId)
+    },
+
+    Company: {
+        jobs: (company) => Job.findAll((job) => job.companyId === company.id)
     }
 }
