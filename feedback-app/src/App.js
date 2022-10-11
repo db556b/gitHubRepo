fc94c9a3129158415ad8d1203c487fe9f9fed951
db556b <davidbuchan900@gmail.com>
2022-10-11 10:31:39 -0400

    learning react

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import Header from './Components/Header';
+import FeedbackItem from './Components/FeedbackItem';
 
 function App() {
   return (
     <>
       <Header text="Hello World"/>
-
+      <FeedbackItem />
     </>
   )
 }
